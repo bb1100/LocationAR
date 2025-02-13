00c21683595a251fe6be6967afd6bffb2d45a414
BB <balrajbains@sky.com>
2025-02-13 21:48:11 +0000

    a-asset-item

--- basic.js
+++ basic.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     let testEntityAdded = false;
+    let latOffset = 0.001;
 
     const el = document.querySelector("[gps-new-camera]");
 
@@ -10,20 +11,29 @@ window.onload = () => {
     el.addEventListener("gps-camera-update-position", e => {
         if(!testEntityAdded) {
             alert(`Got first GPS position: lon ${e.detail.position.longitude} lat ${e.detail.position.latitude}`);
-            // Add a box to the north of the initial GPS position
-            const entity = document.createElement("a-box");
+
+
+            const entity = document.createElement("a-asset-item");
             entity.setAttribute("scale", {
                 x: 20, 
                 y: 20,
                 z: 20
             });
-            entity.setAttribute('material', { color: 'red' } );
+            // entity.setAttribute('material', { color: 'red' } );
             entity.setAttribute('gps-new-entity-place', {
-                latitude: e.detail.position.latitude + 0.001,
+                latitude: e.detail.position.latitude + latOffset,
                 longitude: e.detail.position.longitude
             });
             document.querySelector("a-scene").appendChild(entity);
         }
         testEntityAdded = true;
+
+
+        function floatAnimation() {
+            t += 0.01;
+            requestAnimationFrame(floatAnimation);
+            entity.setAttribute("position", "3 " + (Math.sin(t * 2) + 1) + " 0");
+          }
+        floatAnimation();
     });
 };
