fe8a4926d6800f97f371871ef827aa37f8de338d
BB <balrajbains@sky.com>
2025-02-14 02:37:23 +0000

    en.pos

--- basic.js
+++ basic.js
@@ -40,7 +40,7 @@ window.onload = () => {
             });
             // entity.setAttribute("position", {x: 0, y: 30, z: 0});
             entity.setAttribute("animation", {
-                property: position.y, from: 0, to: 30, 
+                property: entity.position.y, from: 0, to: 30, 
                 dur: 2000, easing: easeOutCubic, loop: true});
             document.querySelector("a-scene").appendChild(entity);
         }
