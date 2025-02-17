10b37c72dd34cc40d68773a75157c6a6a5f5d8cb
BB <balrajbains@sky.com>
2025-02-17 06:17:05 +0000

    play anim

--- basic.js
+++ basic.js
@@ -86,7 +86,7 @@ window.onload = () => {
                 latitude: mapLatitude + latOffset,
                 longitude: mapLongitude + 0.001
             });
-            // entity2.setAttribute("animation-mixer", "");
+            entity2.setAttribute("animation-mixer", "");
         
             document.querySelector("a-scene").appendChild(entity2);
 
