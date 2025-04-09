993f745b06085d660367b5b6d59c5c5c5a1669d4
BB <balrajbains@sky.com>
2025-04-09 01:49:59 +0100

    renderer fix z fighting and cubemap application

--- basic.js
+++ basic.js
@@ -1,3 +1,9 @@
+// Workaround for an AR.js bug (https://github.com/jeromeetienne/AR.js/issues/410)
+const sceneEl = document.querySelector('a-scene');
+sceneEl.addEventListener('loaded', () => {
+  sceneEl.camera = new THREE.PerspectiveCamera();
+});
+
 // /*AFRAME.registerComponent('float-animation', {
 //     schema: {
 //       to: { type: 'number', default: 10 }, // End height (Y-axis)
